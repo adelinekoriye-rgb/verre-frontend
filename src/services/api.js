a9221da4b8c6430d22481668013ac3f322f8f8c1
Koriye <adelinekoriye@gmail.com>
2026-07-31 10:18:35 +0100

    Ajout config production API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5000',
+ const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
 });
 
 // Ajoute automatiquement le token à chaque requête si l'utilisateur est connecté
